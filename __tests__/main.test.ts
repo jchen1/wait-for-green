18f2509f31a07e843ef6bf4529e7eceb5b523b38
Jeff Chen <hello@jeff.yt>
2022-04-21 05:49:35 -0500

    Test runner

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,7 +1,3 @@
-import * as process from 'process';
-import * as cp from 'child_process';
-import * as path from 'path';
-import {expect, test} from '@jest/globals';
+import {expect, test, describe, it} from '@jest/globals';
 
-// shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {});
